b6b60b3aa7410965095bf449a64020f245741837
ali saghir <alisaghir543@gmail.com>
2024-12-13 10:34:46 +0400

    translated the terms

---
+++ app/translation/termsTrans.js
@@ -0,0 +1,182 @@
+const termsTrans = {
+    en: {
+      title: "Privacy Policy",
+      introduction:
+        "Thank you for viewing Meat Moot’s (“MeatMoot’s,” “our,” “us,” “we”) privacy policy...",
+      sections: {
+        informationCollection: {
+          title: "Information Collection",
+          content: [
+            "When you use our services, we, or service providers acting on our behalf, may collect your information...",
+            "There are some situations in which it is necessary for us to collect your information...",
+            "We may contact you via email in the future to tell you about specials...",
+          ],
+        },
+        informationUse: {
+          title: "Information Use",
+          content: [
+            "We use the collected information for various purposes such as improving our operations...",
+          ],
+        },
+        security: {
+          title: "Security",
+          content: [
+            "To protect the information submitted to us, we adhere to generally accepted industry standards...",
+            "If we collect sensitive information (such as credit card information), it is encrypted...",
+            "Hyperlinks to other internet resources are at your own risk...",
+          ],
+        },
+        retention: {
+          title: "How long do we retain your data?",
+          content: [
+            "We keep the information you provide directly when subscribing in our systems for the duration of your membership...",
+          ],
+        },
+        links: {
+          title: "Links",
+          content: [
+            "This Website may contain links to other sites...",
+          ],
+        },
+        disclosure: {
+          title: "Disclosure of personal information",
+          content: [
+            "Meatmoot will not disclose personal data without consent except when required by law...",
+          ],
+        },
+        childrenPrivacy: {
+          title: "Children’s Privacy",
+          content: [
+            "Our Service does not address anyone under the age of 18...",
+          ],
+        },
+        contactUs: {
+          title: "Contact Us",
+          content: [
+            "If you have any questions regarding this privacy policy you may email us or call our Customer Care line...",
+          ],
+        },
+      },
+    },
+    ru: {
+      title: "Политика конфиденциальности",
+      introduction:
+        "Благодарим вас за просмотр политики конфиденциальности Meat Moot («MeatMoot», «наш», «нас», «мы»)...",
+      sections: {
+        informationCollection: {
+          title: "Сбор информации",
+          content: [
+            "Когда вы пользуетесь нашими услугами, мы или поставщики услуг, действующие от нашего имени...",
+            "В некоторых ситуациях необходимо собирать вашу информацию для предоставления услуг...",
+            "Мы можем связаться с вами по электронной почте, чтобы сообщить о специальных предложениях...",
+          ],
+        },
+        informationUse: {
+          title: "Использование информации",
+          content: [
+            "Мы используем собранную информацию для различных целей, таких как улучшение наших операций...",
+          ],
+        },
+        security: {
+          title: "Безопасность",
+          content: [
+            "Чтобы защитить предоставленную нам информацию, мы соблюдаем общепринятые отраслевые стандарты...",
+            "Если мы собираем конфиденциальную информацию (например, данные кредитной карты)...",
+            "Гиперссылки на другие интернет-ресурсы используются на ваш страх и риск...",
+          ],
+        },
+        retention: {
+          title: "Сколько времени мы храним ваши данные?",
+          content: [
+            "Мы храним информацию, которую вы предоставляете при подписке, в нашей системе на протяжении вашего членства...",
+          ],
+        },
+        links: {
+          title: "Ссылки",
+          content: [
+            "Этот веб-сайт может содержать ссылки на другие сайты...",
+          ],
+        },
+        disclosure: {
+          title: "Раскрытие персональной информации",
+          content: [
+            "Meatmoot не будет раскрывать личные данные без согласия, кроме случаев, предусмотренных законом...",
+          ],
+        },
+        childrenPrivacy: {
+          title: "Конфиденциальность детей",
+          content: [
+            "Наши услуги не предназначены для лиц младше 18 лет...",
+          ],
+        },
+        contactUs: {
+          title: "Свяжитесь с нами",
+          content: [
+            "Если у вас есть вопросы по поводу этой политики конфиденциальности, вы можете написать нам...",
+          ],
+        },
+      },
+    },
+    ar: {
+      title: "سياسة الخصوصية",
+      introduction:
+        "شكرًا لك على عرض سياسة الخصوصية الخاصة بـ Meat Moot (“MeatMoot”, “نحن”)...",
+      sections: {
+        informationCollection: {
+          title: "جمع المعلومات",
+          content: [
+            "عند استخدامك لخدماتنا، قد نقوم نحن أو مزودو الخدمات بجمع معلوماتك...",
+            "في بعض الحالات، من الضروري جمع معلوماتك لتقديم الخدمة...",
+            "قد نتواصل معك عبر البريد الإلكتروني مستقبلاً لإخبارك بالعروض الخاصة...",
+          ],
+        },
+        informationUse: {
+          title: "استخدام المعلومات",
+          content: [
+            "نستخدم المعلومات التي تم جمعها لأغراض متعددة مثل تحسين عملياتنا...",
+          ],
+        },
+        security: {
+          title: "الأمان",
+          content: [
+            "لحماية المعلومات التي تقدمها لنا، نلتزم بالمعايير الصناعية المقبولة...",
+            "إذا قمنا بجمع معلومات حساسة (مثل معلومات بطاقة الائتمان)...",
+            "الروابط إلى موارد الإنترنت الأخرى تكون على مسؤوليتك الخاصة...",
+          ],
+        },
+        retention: {
+          title: "إلى متى نحتفظ ببياناتك؟",
+          content: [
+            "نحتفظ بالمعلومات التي تقدمها عند الاشتراك في أنظمتنا طوال مدة اشتراكك...",
+          ],
+        },
+        links: {
+          title: "الروابط",
+          content: [
+            "قد يحتوي هذا الموقع على روابط لمواقع أخرى...",
+          ],
+        },
+        disclosure: {
+          title: "الإفصاح عن المعلومات الشخصية",
+          content: [
+            "لن تفصح Meatmoot عن البيانات الشخصية بدون موافقة، إلا عند الضرورة القانونية...",
+          ],
+        },
+        childrenPrivacy: {
+          title: "خصوصية الأطفال",
+          content: [
+            "لا تستهدف خدماتنا أي شخص دون سن 18 عامًا...",
+          ],
+        },
+        contactUs: {
+          title: "اتصل بنا",
+          content: [
+            "إذا كانت لديك أي أسئلة حول سياسة الخصوصية هذه، يمكنك مراسلتنا...",
+          ],
+        },
+      },
+    },
+  };
+  
+  export default termsTrans;
+  
