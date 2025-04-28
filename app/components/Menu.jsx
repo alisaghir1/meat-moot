25f88ddcba5758fb464c755b5de509cd448b94b2
ali saghir <alisaghir543@gmail.com>
2025-04-28 16:12:03 +0300

    added another menu the burger menu to the website

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -2,53 +2,44 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { fadeIn } from "@/variants";
-import menuTrans from "../translation/menuTrans.js"; // Import the translation object
+import menuTrans from "../translation/menuTrans.js";
 import { useAppContext } from "../context";
 import heroTrans from "../translation/heroTrans.js";
+import { useState } from "react";
+import { Button } from "./ui/button.jsx";
 
 const Menu = () => {
   const [languege] = useAppContext();
+  const [menuType, setMenuType] = useState("meat"); // "meat" or "burgers"
 
-  const menu = [
-    {
-      img: "/menu/lambShanks.webp",
-      key: "lambShank",
-    },
-    {
-      img: "/menu/lambNeck.webp",
-      key: "lambNeck",
-    },
-    {
-      img: "/menu/lambShoulder.webp",
-      key: "lambShoulder",
-    },
-    {
-      img: "/menu/lambRibs.webp",
-      key: "lambRibs",
-    },
-    {
-      img: "/menu/beefRibs.webp",
-      key: "beefRibs",
-    },
-    {
-      img: "/menu/beefBrisket.webp",
-      key: "beefBrisket",
-    },
-    {
-      img: "/menu/meat1.webp",
-      key: "beefShank",
-    },
+  const meatMenu = [
+    { img: "/menu/lambShanks.webp", key: "lambShank" },
+    { img: "/menu/lambNeck.webp", key: "lambNeck" },
+    { img: "/menu/lambShoulder.webp", key: "lambShoulder" },
+    { img: "/menu/lambRibs.webp", key: "lambRibs" },
+    { img: "/menu/beefRibs.webp", key: "beefRibs" },
+    { img: "/menu/beefBrisket.webp", key: "beefBrisket" },
+    { img: "/menu/meat1.webp", key: "beefShank" },
   ];
 
-  
+  const burgerMenu = [
+    { img: "/menu/UrgeBurger.jpeg", key: "burger1" },
+    { img: "/menu/DoubleUrge.jpeg", key: "burger2" },
+    { img: "/menu/UrgeMushroom.jpeg", key: "burger3" },
+    { img: "/menu/DoubleUrgeMushroom.jpeg", key: "burger4" },
+    { img: "/menu/UrgeMaxDouble.jpeg", key: "burger5" },
+    { img: "/menu/MiniUrgeKids.jpg", key: "burger6" },
+  ];
+
+  const menuToShow = menuType === "meat" ? meatMenu : burgerMenu;
 
   return (
-    <section className="bg-black relative py-12 xl:py-24 " id="menu">
-      <div className="container mx-auto ">
+    <section className="bg-black relative py-12 xl:py-24" id="menu">
+      <div className="container mx-auto">
         <motion.div
           variants={fadeIn("left", 0.6)}
           initial="hidden"
-          whileInView={"show"}
+          whileInView="show"
           viewport={{ once: false, amount: 0.4 }}
           className={`mb-5 max-w-[2000px] mx-auto ${
             languege === "ar" ? "xl:text-right" : "text-center"
@@ -58,19 +49,53 @@ const Menu = () => {
             <h2 className="text-orange mt-2">
               {menuTrans[languege].conceptTitle}
             </h2>
-            <p className="text-white">{menuTrans[languege].conceptDescription}</p>
+            <p className="text-white">
+              {menuTrans[languege].conceptDescription}
+            </p>
           </div>
         </motion.div>
 
+        {/* Menu Switch Buttons */}
+        <div className="flex xl:justify-start justify-center gap-4 mb-8">
+          <Button
+            onClick={() => setMenuType("meat")}
+            className={`px-6 py-2 border border-orange rounded-lg hover:bg-black-heavy hover:scale-105 transition-all duration-200 ${
+              menuType === "meat"
+                ? "bg-orange text-white"
+                : "bg-black text-white"
+            }`}
+          >
+            {languege === "ar" ? "لحوم" : languege === "ru" ? "Мясо" : "Meat"}
+          </Button>
+
+          <Button
+            onClick={() => setMenuType("burgers")}
+            className={`px-6 py-2 border border-orange rounded-lg hover:bg-black-heavy hover:scale-105 transition-all duration-200 ${
+              menuType === "burgers"
+                ? "bg-orange text-white"
+                : "bg-black text-white"
+            }`}
+          >
+            {languege === "ar"
+              ? "برجر"
+              : languege === "ru"
+              ? "Бургеры"
+              : "Burgers"}
+          </Button>
+        </div>
+
+        {/* Menu Items */}
         <div className="flex flex-col gap-5">
-          {menu.map((item, index) => {
+          {menuToShow.map((item, index) => {
             const translatedItem = menuTrans[languege][item.key];
-            const flexDirection = index % 2 === 0 ? "xl:flex-row" : "xl:flex-row-reverse";
+            const flexDirection =
+              index % 2 === 0 ? "xl:flex-row" : "xl:flex-row-reverse";
+
             return (
               <motion.div
                 variants={fadeIn("left", 0.6)}
                 initial="hidden"
-                whileInView={"show"}
+                whileInView="show"
                 viewport={{ once: false, amount: 0 }}
                 key={index}
                 className={`flex flex-col ${flexDirection} shadow-primary mx-auto xl:mx-0 group mt-5`}
@@ -96,16 +121,26 @@ const Menu = () => {
                   </div>
                   <div className="xl:text-xl text-lg text-white font-semibold">
                     {translatedItem.price}
+                    {translatedItem.comboPrice && (
+                      <>
+                        <span className="text-gray-500 mx-2">|</span>
+                        <span className="text-orange">
+                          {languege === "ar" ? "كومبو: " : "Combo: "}
+                          {translatedItem.comboPrice}
+                        </span>
+                      </>
+                    )}
                   </div>
                 </div>
               </motion.div>
             );
           })}
         </div>
+
         <motion.p
           variants={fadeIn("down", 0.8)}
           initial="hidden"
-          whileInView={"show"}
+          whileInView="show"
           viewport={{ once: false, amount: 0.4 }}
           className="mb-7 text-xl xl:text-2xl text-orange font-base mt-10 text-center"
         >
--- app/translation/menuTrans.js
+++ app/translation/menuTrans.js
@@ -39,6 +39,42 @@ const menuTrans = {
         description: "Tender Smoked Shank. It comes with 8 sauces, 6 Salads, and a beverage of your choice.",
         price: "AED 436.5/KG (excl.VAT)"
       },
+      burger1: {
+        title: "Double Urge Burger",
+        description: "Double the beef, double the crave! Made with two smashed beef patties, totalling 130g of juicy goodness, layered with melted cheddar, creamy truffle sauce, and rich cheese sauce, all tucked into toasted buttery buns. Finished with fresh tomato and tangy pickles for a bold, flavor-packed bite in every layer.",
+        price: "35.75 Dhs",
+        comboPrice: "36.25 Dhs"
+      },
+      burger2: {
+        title: "Double Urge Burger (Large)",
+        description: "Double the beef, double the crave! Made with two smashed beef patties, totalling 130g of juicy goodness, layered with melted cheddar, creamy truffle sauce, and rich cheese sauce, all tucked into toasted buttery buns. Finished with fresh tomato and tangy pickles for a bold, flavor-packed bite in every layer.",
+        price: "40 Dhs",
+        comboPrice: "46.75 Dhs"
+      },
+      burger3: {
+        title: "Urge Mushroom Burger",
+        description: "A rich and savory twist on our signature! An 80g juicy smashed beef patty layered with melted cheddar, velvety cheese sauce, and earthy mushroom sauce, topped with sautéed mushrooms and truffle sauce, all hugged by golden buttery buns. Served with fresh tomato and tangy pickles for a full-flavored, crave-worthy experience.",
+        price: "44 Dhs",
+        comboPrice: "49 Dhs"
+      },
+      burger4: {
+        title: "Double Urge Mushroom Burger",
+        description: "Double the patties, double the flavor! Two juicy smashed beef patties, totaling 160g, stacked with melted cheddar, creamy cheese sauce, rich mushroom sauce, and layers of sautéed mushrooms, finished with aromatic truffle sauce and tucked inside golden, buttery buns. Served with fresh tomato and tangy pickles, it’s a bold, indulgent bite for serious burger lovers.",
+        price: "50.5 Dhs",
+        comboPrice: "54 Dhs"
+      },
+      burger5: {
+        title: "Urge Max Burger – Double",
+        description: "Sink your teeth into the ultimate indulgence: two juicy smashed beef patties, weighing 200g in total, layered with melted cheddar, rich cheese sauce, and a velvety hint of truffle sauce, all nestled in golden, buttery toasted buns. Served with fresh tomato and tangy pickles on the side, the Urge Max Burger is bold, beefy, and built to satisfy your biggest cravings.",
+        price: "65 Dhs",
+        comboPrice: "70 Dhs"
+      },
+      burger6: {
+        title: "Mini Urge Burger – Kids",
+        description: "A tasty treat just for kids! This meal includes two mini burgers, each with a juicy 35g beef patty, a slice of cheddar cheese, and creamy cheddar cheese sauce, all on soft kid-sized buns with a touch of truffle sauce. Served with fresh tomato on the side for a little extra crunch if they like!",
+        price: "35.75 Dhs",
+        comboPrice: "40 Dhs"
+      }
     },
     ru: 
     {
@@ -80,6 +116,43 @@ const menuTrans = {
         description: "Нежная копченая голяшка. Подается с 8 соусами, 6 салатами и напитком на выбор.",
         price: "436,5 AED/кг (без НДС)"
       },
+      burger1: {
+        title: "Бургер Double Urge",
+        description: "Вдвое больше говядины, вдвое больше удовольствия! Приготовлен из двух smashed говяжьих котлет общим весом 130 г, с расплавленным чеддером, сливочным трюфельным соусом и насыщенным сырным соусом в поджаренных масляных булочках. Завершается свежими помидорами и кислыми огурчиками для мощного вкуса в каждом слое.",
+        price: "35.75 دерхамов",
+        comboPrice: "36.25 دерхамов"
+      },
+      burger2: {
+        title: "Бургер Double Urge (Большой)",
+        description: "Вдвое больше говядины, вдвое больше удовольствия! Приготовлен из двух smashed говяжьих котлет общим весом 130 г, с расплавленным чеддером, сливочным трюфельным соусом и насыщенным сырным соусом в поджаренных масляных булочках. Завершается свежими помидорами и кислыми огурчиками для мощного вкуса в каждом слое.",
+        price: "40 دерхамов",
+        comboPrice: "46.75 دерхамов"
+      },
+      burger3: {
+        title: "Бургер Urge с грибами",
+        description: "Богатая и насыщенная версия нашего фирменного бургера! 80-граммовая smashed котлета из говядины, расплавленный чеддер, нежный сырный соус и грибной соус с обжаренными грибами и трюфельным соусом — всё в золотистых булочках с маслом. Подается со свежими помидорами и кислыми огурчиками для полного вкусового наслаждения.",
+        price: "44 دерхамов",
+        comboPrice: "49 دерхамов"
+      },
+      burger4: {
+        title: "Double Urge с грибами",
+        description: "Две котлеты — двойной вкус! Две smashed котлеты из говядины общим весом 160 г, с расплавленным чеддером, сливочным сырным соусом, грибным соусом и обжаренными грибами, дополненные ароматным трюфельным соусом в масляных булочках. Подается со свежими помидорами и кислыми огурцами — бургер для настоящих гурманов.",
+        price: "50.5 دерхамов",
+        comboPrice: "54 دерхамов"
+      },
+      burger5: {
+        title: "Urge Max Бургер – Двойной",
+        description: "Погрузитесь в максимальное наслаждение: две smashed котлеты из говядины весом 200 г, с расплавленным чеддером, насыщенным сырным соусом и легким трюфельным акцентом в масляных поджаренных булочках. Подается со свежими помидорами и кислыми огурцами. Urge Max — насыщенный, мясной и идеальный для больших аппетитов.",
+        price: "65 دерхамов",
+        comboPrice: "70 دерхамов"
+      },
+      burger6: {
+        title: "Mini Urge Бургер – Для детей",
+        description: "Вкусное угощение для детей! В набор входят два мини-бургера: каждая булочка с 35-граммовой котлеткой из говядины, ломтиком чеддера и сливочным сырным соусом. Мягкие детские булочки с легким трюфельным акцентом. Подается со свежими помидорами — по желанию!",
+        price: "35.75 دерхамов",
+        comboPrice: "40 دерхамов"
+      }
+      
     },
     ar: {
       menuTitle: ' قائمتنا',
@@ -120,6 +193,43 @@ const menuTrans = {
         description: "عقب لحم بقري مدخن طري. تأتي مع 8 صلصات، 6 سلطات، ومشروب من اختيارك.",
         price: "436.5 AED/كجم (باستثناء ضريبة القيمة المضافة)"
       },
+      burger1: {
+        title: "برجر دبل أورج",
+        description: "ضعف اللحم، ضعف اللذة! مصنوع من قطعتين من لحم البقر المشوي المهروس بوزن إجمالي 130 جرام، مع جبنة شيدر ذائبة، صلصة الترفل الكريمية، وصلصة الجبن الغنية، كلها داخل خبز محمص طري بالزبدة. يكتمل بشرائح طماطم طازجة ومخللات لاذعة لمذاق جريء ومميز في كل قضمة.",
+        price: "35.75 درهم",
+        comboPrice: "36.25 درهم"
+      },
+      burger2: {
+        title: "برجر دبل أورج (كبير)",
+        description: "ضعف اللحم، ضعف اللذة! مصنوع من قطعتين من لحم البقر المشوي المهروس بوزن إجمالي 130 جرام، مع جبنة شيدر ذائبة، صلصة الترفل الكريمية، وصلصة الجبن الغنية، كلها داخل خبز محمص طري بالزبدة. يكتمل بشرائح طماطم طازجة ومخللات لاذعة لمذاق جريء ومميز في كل قضمة.",
+        price: "40 درهم",
+        comboPrice: "46.75 درهم"
+      },
+      burger3: {
+        title: "برجر أورج بالمشروم",
+        description: "لمسة غنية ومميزة على البرجر الكلاسيكي! قطعة لحم بقر مشوي مهروس بوزن 80 جرام، مغطاة بجبنة شيدر ذائبة، صلصة جبن ناعمة، وصلصة فطر لذيذة، topped بفطر مقلي وصلصة الترفل، داخل خبز ذهبي بالزبدة. يُقدم مع طماطم طازجة ومخللات لاذعة لمذاق كامل ومشبع.",
+        price: "44 درهم",
+        comboPrice: "49 درهم"
+      },
+      burger4: {
+        title: "برجر دبل أورج بالمشروم",
+        description: "قطعتان من اللحم، ضعف المذاق! قطعتان من لحم البقر المشوي المهروس بوزن إجمالي 160 جرام، مع جبنة شيدر ذائبة، صلصة جبن كريمية، صلصة مشروم غنية، وطبقات من الفطر المقلي، مغطاة بصلصة الترفل داخل خبز ذهبي طري بالزبدة. يُقدم مع طماطم طازجة ومخللات لاذعة — تجربة جريئة لعشاق البرجر.",
+        price: "50.5 درهم",
+        comboPrice: "54 درهم"
+      },
+      burger5: {
+        title: "برجر أورج ماكس – دبل",
+        description: "اغمر حواسك بأقصى درجات المتعة: قطعتان من لحم البقر المشوي المهروس بوزن 200 جرام، مع جبنة شيدر ذائبة، صلصة جبن غنية، ولمسة ناعمة من صلصة الترفل، كلها داخل خبز محمص ذهبي بالزبدة. يُقدم مع طماطم طازجة ومخللات لاذعة، برجر أورج ماكس قوي، لذيذ، ومخصص لمن لديهم شهية كبيرة.",
+        price: "65 درهم",
+        comboPrice: "70 درهم"
+      },
+      burger6: {
+        title: "ميني أورج برجر – للأطفال",
+        description: "وجبة شهية للأطفال فقط! تتضمن هذه الوجبة قطعتين من البرجر المصغر، كل منها يحتوي على قطعة لحم بقر مشوي بوزن 35 جرام، شريحة من جبنة الشيدر، وصلصة جبنة كريمية، كلها داخل خبز صغير طري مع لمسة من صلصة الترفل. يُقدم مع طماطم طازجة لمن يحب القرمشة الإضافية!",
+        price: "35.75 درهم",
+        comboPrice: "40 درهم"
+      }
+      
     },
   };
   
