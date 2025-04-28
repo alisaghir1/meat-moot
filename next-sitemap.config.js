f6b1121d75f355096cf6e9cdd15962afaa2774f7
ali saghir <alisaghir543@gmail.com>
2025-04-28 16:14:21 +0300

    added a sitemap and robust.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://meatmoot.ae', // 🔥 Replace with your actual domain
+    generateRobotsTxt: true, // (Optional) will generate robots.txt
+    sitemapSize: 7000,
+  };
+  
