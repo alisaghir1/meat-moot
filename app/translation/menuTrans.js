db7d88e255387c9b3f3a2a5a8073f9dbb4e52f24
alisaghir1 <alisaghir543@gmail.com>
2025-07-08 14:48:39 +0300

    edited the prices of the menu

--- app/translation/menuTrans.js
+++ app/translation/menuTrans.js
@@ -7,37 +7,37 @@ const menuTrans = {
       lambShank: {
         title: "Lamb Shank",
         description: "The finest, and tastiest smoked shanks.it comes with 8 sauces, 6 Salads and beverage of your choice.",
-        price: "AED 436.5/KG (excl.VAT)"
+        price: "AED 450 Dhs/kg (excl.VAT)"
       },
       lambNeck: {
         title: "Lamb Neck",
         description: "A classic smoked neck meat. It comes with 8 sauces, 6 Salads, and a beverage of your choice.",
-        price: "AED 436.5/KG (excl.VAT)"
+        price: "AED 450 Dhs/kg (excl.VAT)"
       },
       lambShoulder: {
         title: "Lamb Shoulder",
         description: "An authentic smoked meat. It comes with 8 sauces, 6 Salads, and a beverage of your choice.",
-        price: "AED 436.5/KG (excl.VAT)"
+        price: "AED 450 Dhs/kg (excl.VAT)"
       },
       lambRibs: {
         title: "Lamb Ribs",
         description: "The finest smoked lamb ribs. It comes with 8 sauces, 6 Salads, and a beverage of your choice.",
-        price: "AED 436.5/KG (excl.VAT)"
+        price: "AED 450 Dhs/kg (excl.VAT)"
       },
       beefRibs: {
         title: "Beef Ribs",
         description: "Juicy and flavorful ribs. It comes with 8 sauces, 6 Salads, and a beverage of your choice.",
-        price: "AED 436.5/KG (excl.VAT)"
+        price: "AED 450 Dhs/kg (excl.VAT)"
       },
       beefBrisket: {
         title: "Beef Brisket",
         description: "Tender Smoked Brisket. It comes with 8 sauces, 6 Salads, and a beverage of your choice.",
-        price: "AED 436.5/KG (excl.VAT)"
+        price: "AED 450 Dhs/kg (excl.VAT)"
       },
       beefShank: {
         title: "Beef Shank",
         description: "Tender Smoked Shank. It comes with 8 sauces, 6 Salads, and a beverage of your choice.",
-        price: "AED 436.5/KG (excl.VAT)"
+        price: "AED 450 Dhs/kg (excl.VAT)"
       },
       burger1: {
         title: "Urge Burger",
@@ -84,37 +84,37 @@ const menuTrans = {
       lambShank: {
         title: "Бараний голяшка",
         description: "Лучшие и самые вкусные копченые голяшки. Подается с 8 соусами, 6 салатами и напитком на выбор.",
-        price: "436,5 AED/кг (без НДС)"
+        price: "450 AED/кг (без НДС)"
       },
       lambNeck: {
         title: "Бараний шейка",
         description: "Классическое копченое мясо шеи. Подается с 8 соусами, 6 салатами и напитком на выбор.",
-        price: "436,5 AED/кг (без НДС)"
+        price: "450 AED/кг (без НДС)"
       },
       lambShoulder: {
         title: "Бараний плечо",
         description: "Аутентичное копченое мясо. Подается с 8 соусами, 6 салатами и напитком на выбор.",
-        price: "436,5 AED/кг (без НДС)"
+        price: "450 AED/кг (без НДС)"
       },
       lambRibs: {
         title: "Бараний ребра",
         description: "Лучшие копченые ребра баранины. Подается с 8 соусами, 6 салатами и напитком на выбор.",
-        price: "436,5 AED/кг (без НДС)"
+        price: "450 AED/кг (без НДС)"
       },
       beefRibs: {
         title: "Говяжьи ребра",
         description: "Сочные и ароматные ребра. Подается с 8 соусами, 6 салатами и напитком на выбор.",
-        price: "436,5 AED/кг (без НДС)"
+        price: "450 AED/кг (без НДС)"
       },
       beefBrisket: {
         title: "Говяжья грудинка",
         description: "Нежная копченая грудинка. Подается с 8 соусами, 6 салатами и напитком на выбор.",
-        price: "436,5 AED/кг (без НДС)"
+        price: "450 AED/кг (без НДС)"
       },
       beefShank: {
         title: "Говяжий голяшка",
         description: "Нежная копченая голяшка. Подается с 8 соусами, 6 салатами и напитком на выбор.",
-        price: "436,5 AED/кг (без НДС)"
+        price: "450 AED/кг (без НДС)"
       },
       burger1: {
         title: "Урдж Бургер",
@@ -161,37 +161,37 @@ const menuTrans = {
       lambShank: {
         title: "عقب الغنم",
         description: "أفضل وأشهى أضلاع الغنم المدخنة. تأتي مع 8 صلصات، 6 سلطات، ومشروب من اختيارك.",
-        price: "436.5 AED/كجم (باستثناء ضريبة القيمة المضافة)"
+        price: "450 AED/كغ (باستثناء ضريبة القيمة المضافة)"
       },
       lambNeck: {
         title: "عنق الغنم",
         description: "لحمة عنق الغنم المدخنة الكلاسيكية. تأتي مع 8 صلصات، 6 سلطات، ومشروب من اختيارك.",
-        price: "436.5 AED/كجم (باستثناء ضريبة القيمة المضافة)"
+        price: "450 AED/كغ (باستثناء ضريبة القيمة المضافة)"
       },
       lambShoulder: {
         title: "كتف الغنم",
         description: "لحمة مدخنة أصلية. تأتي مع 8 صلصات، 6 سلطات، ومشروب من اختيارك.",
-        price: "436.5 AED/كجم (باستثناء ضريبة القيمة المضافة)"
+        price: "450 AED/كغ (باستثناء ضريبة القيمة المضافة)"
       },
       lambRibs: {
         title: "ضلوع الغنم",
         description: "أفضل ضلوع الغنم المدخنة. تأتي مع 8 صلصات، 6 سلطات، ومشروب من اختيارك.",
-        price: "436.5 AED/كجم (باستثناء ضريبة القيمة المضافة)"
+        price: "450 AED/كغ (باستثناء ضريبة القيمة المضافة)"
       },
       beefRibs: {
         title: "ضلوع اللحم البقري",
         description: "ضلوع لحم بقري طرية ولذيذة. تأتي مع 8 صلصات، 6 سلطات، ومشروب من اختيارك.",
-        price: "436.5 AED/كجم (باستثناء ضريبة القيمة المضافة)"
+        price: "450 AED/كغ (باستثناء ضريبة القيمة المضافة)"
       },
       beefBrisket: {
         title: "صدر اللحم البقري",
         description: "صدر لحم بقري مدخن طري. تأتي مع 8 صلصات، 6 سلطات، ومشروب من اختيارك.",
-        price: "436.5 AED/كجم (باستثناء ضريبة القيمة المضافة)"
+        price: "450 AED/كغ (باستثناء ضريبة القيمة المضافة)"
       },
       beefShank: {
         title: "عقب اللحم البقري",
         description: "عقب لحم بقري مدخن طري. تأتي مع 8 صلصات، 6 سلطات، ومشروب من اختيارك.",
-        price: "436.5 AED/كجم (باستثناء ضريبة القيمة المضافة)"
+        price: "450 AED/كغ (باستثناء ضريبة القيمة المضافة)"
       },
       burger1: {
         title: "أورج برجر",
